dcb940ae1cb77f7b64128e641d8e73a6bef34060
ip11 <iprash011@gmail.com>
2020-06-12 19:10:15 +0530

    default Props and propTypes

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,12 +1,23 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 class NavBar extends Component {
+	static defaultProps = {
+		title: 'GitHub Project',
+		icon: 'fab fa-github'
+	};
+
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		icon: PropTypes.string.isRequired
+	};
+
 	render() {
 		return (
 			<Fragment>
 				<nav className="navbar bg-primary">
 					<h1>
-						<i className="fab fa-github"> </i> GitHub Project
+						<i className={this.props.icon}> </i> {this.props.title}
 					</h1>
 				</nav>
 			</Fragment>
