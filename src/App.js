f399165b6feb6506a989cccabcc72072582358a6
ip11 <iprash011@gmail.com>
2020-06-15 13:47:13 +0530

    Built UserItem component with fixed state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import NavBar from './components/layouts/NavBar';
+import UserItem from './components/UserItem';
 
 const App = () => {
 	return (
 		<Fragment>
 			<NavBar />
+			<UserItem />
 		</Fragment>
 	);
 };
---
+++ src/components/UserItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+	state = {
+		id: 'id',
+		login: 'ip11',
+		avatar_url: 'https://avatars0.githubusercontent.com/u/39377668?v=4',
+		html_url: 'https://github.com/ip11'
+	};
+	render() {
+		return (
+			<div className="card text-center">
+				<img src={this.state.avatar_url} alt="" className="round-img" style={{ width: '120 px' }} />
+				<h3> {this.state.login}</h3>
+				<div>
+					<a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+						GitHub Profile
+					</a>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default UserItem;
