5a04c7f4b3ef653c3a1835cc340c06076bbd49a4
ip11 <iprash011@gmail.com>
2020-06-15 14:29:54 +0530

    added grids

--- src/components/Users.js
+++ src/components/Users.js
@@ -26,8 +26,13 @@ class Users extends Component {
 	};
 
 	render() {
-		return <div>{this.state.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
+		return <div style={styles}>{this.state.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
 	}
 }
 
+const styles = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3,1fr)',
+	gridGap: '1rem'
+};
 export default Users;
