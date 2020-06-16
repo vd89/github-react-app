3cdd0ed9351ccd9dc9b4a752e7a22fd0280b3c2a
ip11 <iprash011@gmail.com>
2020-06-16 13:18:29 +0530

    Implemented onChange on Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: ''
+		};
+		this.onChangeHandler = this.onChangeHandler.bind(this);
+	}
+	onChangeHandler(event) {
+		this.setState({
+			text: event.target.value
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<form className="form">
-					<input type="text" name="text" placeholder="Enter Github UserName to Search" />
+					<input
+						type="text"
+						name="text"
+						value={this.state.text}
+						placeholder="Enter Github UserName to Search"
+						onChange={this.onChangeHandler}
+					/>
 					<input type="submit" value="Submit" className="btn btn-dark btn-block" />
 				</form>
 			</div>
