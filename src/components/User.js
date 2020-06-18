f021321adacc0da63b57755f0a47c2facf48e7fa
ip11 <iprash011@gmail.com>
2020-06-18 15:39:04 +0530

    Implement Spinner in user Component

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from './layouts/Spinner';
+import {Link} from 'react-router-dom';
 
-const User = () => {
-	return (
-		<div>
-			<h1> User Component</h1>
-		</div>
-	);
-};
+class User extends Component {
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.login);
+	}
+
+	render() {
+		const {
+			name,
+			avatar_url,
+			location,
+			bio,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+		const { loading } = this.props;
+
+		if (loading) {
+			return <Spinner />;
+		}
+
+		return (
+			<Fragment>
+				<Link to="/" className="btn btn-light">
+					{' '}
+					Back to Search
+				</Link>
+			</Fragment>
+		);
+	}
+}
 
 export default User;
--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user }) => {
-	const { avatar_url, login, html_url } = user;
+	const { avatar_url, login } = user;
 
 	return (
 		<div className="card text-center">
 			<img src={avatar_url} alt="" className="round-img" style={{ width: '120px' }} />
 			<h3> {login}</h3>
 			<div>
-				<a href={html_url} className="btn btn-dark btn-sm my-1">
+				<Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
 					GitHub Profile
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
