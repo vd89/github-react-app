b47c84c908d6c23e9699bc5694d17c144ff95d63
ip11 <iprash011@gmail.com>
2020-06-18 14:34:13 +0530

    Added Link from React Router to Navbar Component

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 const NavBar = ({ title, icon }) => {
 	return (
 		<Fragment>
@@ -10,6 +10,14 @@ const NavBar = ({ title, icon }) => {
 						<i className={icon}> </i> {title}
 					</h1>
 				</a>
+				<ul>
+					<li>
+						<Link to="/"> Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
 			</nav>
 		</Fragment>
 	);
