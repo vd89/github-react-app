cc3b0b8d6ba258fe5e6bf2bcd9a6868630668a38
chiragdoctor <chirag.doctor@gmail.com>
2020-06-18 18:36:44 +0100

    user profile component

--- src/components/User.js
+++ src/components/User.js
@@ -33,6 +33,50 @@ class User extends Component {
 					{' '}
 					Back to Search
 				</Link>
+				<div className="card card-shadow grid-2">
+						<div className="all-center">
+							<img src={avatar_url} alt="GitHub" className="round-img" style={{ width: '300px' }}/>
+						</div>
+						<div>
+							<h1 className="p">{name}</h1>
+							<h3 className="p">{login}</h3>
+							<p className="p">Location: {location}</p>
+							
+							<p className="p">Bio: {bio ? bio: 'Bio not available..'}</p>
+							<div>
+							<p className="p">Hireable  :{' '}
+								{hireable ? (
+									<i className="fas fa-check text-success" />
+								) : (
+									<i className="fas fa-ban text-danger" />
+								)} 
+							</p>
+						</div>
+						<div className="badge card-shadow grid-2" style={{display: 'grid'}}>
+							<div className="text-center">
+								<h3>
+									Followers: {followers}
+								</h3>
+							</div>
+							
+							<div className="text-center">
+								<h3>
+									Following: {following}
+								</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="card card-shadow grid-2">
+					<div className="text-center"> 
+						<h3>Public Repos: {public_repos}</h3>
+					</div>
+					<div className="text-center"> 
+						<h3>Public Gists: {public_gists}</h3>
+					</div>
+				</div>
+				<p><a href={html_url} target="_blank" rel="noopener noreferrer" className="btn btn-dark text-center p-4" style={{width: '100%', padding: '1rem'}}>View Profile</a></p>
+				
 			</Fragment>
 		);
 	}
