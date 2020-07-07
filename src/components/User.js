eb59614e21fe5cf6f2789bc833b0e28b6f65efef
chiragdoctor <chirag.doctor@gmail.com>
2020-07-07 16:28:12 +0100

    convert class based user component with hooks

--- src/components/User.js
+++ src/components/User.js
@@ -1,104 +1,95 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Spinner from './layouts/Spinner';
 import Repos from './Repos';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-class User extends Component {
-	componentDidMount() {
-		this.props.getUser(this.props.match.params.login);
-		this.props.getUserRepos(this.props.match.params.login);
+
+const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+	useEffect(() => {
+		getUser(match.params.login);
+		getUserRepos(match.params.login);
+		//eslint-disable-next-line
+	}, []);
+
+	const {
+		name,
+		avatar_url,
+		location,
+		bio,
+		login,
+		html_url,
+		followers,
+		following,
+		public_repos,
+		public_gists,
+		hireable,
+		company,
+		blog,
+	} = user;
+
+	if (loading) {
+		return <Spinner />;
 	}
 
-	static propTypes = {
-		loading: PropTypes.bool.isRequired,
-		user: PropTypes.object.isRequired,
-		getUser: PropTypes.func.isRequired,
-		getUserRepos: PropTypes.func.isRequired,
-		repos: PropTypes.array.isRequired,
-	};
-	render() {
-		const {
-			name,
-			avatar_url,
-			location,
-			bio,
-			login,
-			html_url,
-			followers,
-			following,
-			public_repos,
-			public_gists,
-			hireable,
-			company,
-			blog,
-		} = this.props.user;
-		const { loading, repos } = this.props;
-
-		if (loading) {
-			return <Spinner />;
-		}
-
-		return (
-			<Fragment>
-				<Link to='/' className='btn btn-light'>
-					{' '}
-					Back to Search
-				</Link>
-				Hireable : {hireable ? <i className='fas fa-check text-success' /> : <i className='fas fa-times-circle text-danger' />}
-				<div className='card grid-2'>
-					<div className='all-center'>
-						<img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
-						<h1>{name}</h1>
-						<p>Location : {location}</p>
-					</div>
-					<div>
-						{bio && (
-							<Fragment>
-								<h3>User Bio</h3>
-								<p>{bio}</p>
-							</Fragment>
-						)}
-						<a href={html_url} className='btn btn-dark my-1 ' target='_blank' rel='noopener noreferrer'>
-							Visit GitHub Profile{' '}
-						</a>
-						<ul>
-							<li>
-								{login && (
-									<Fragment>
-										{' '}
-										UserName :<strong> {login}</strong>{' '}
-									</Fragment>
-								)}
-							</li>
-							<li>
-								{company && (
-									<Fragment>
-										{' '}
-										Company :<strong> {company}</strong>{' '}
-									</Fragment>
-								)}
-							</li>
-							<li>
-								{blog && (
-									<Fragment>
-										{' '}
-										Website :<strong> {blog}</strong>{' '}
-									</Fragment>
-								)}
-							</li>
-						</ul>
-					</div>
+	return (
+		<Fragment>
+			<Link to='/' className='btn btn-light'>
+				{' '}
+				Back to Search
+			</Link>
+			Hireable : {hireable ? <i className='fas fa-check text-success' /> : <i className='fas fa-times-circle text-danger' />}
+			<div className='card grid-2'>
+				<div className='all-center'>
+					<img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
+					<h1>{name}</h1>
+					<p>Location : {location}</p>
 				</div>
-				<div className='card text-center'>
-					<div className='badge badge-primary'>Followers : {followers}</div>
-					<div className='badge badge-success'>Following : {following}</div>
-					<div className='badge badge-light'>Public Repositories : {public_repos}</div>
-					<div className='badge badge-dark'>Public Gists : {public_gists}</div>
+				<div>
+					{bio && (
+						<Fragment>
+							<h3>User Bio</h3>
+							<p>{bio}</p>
+						</Fragment>
+					)}
+					<a href={html_url} className='btn btn-dark my-1 ' target='_blank' rel='noopener noreferrer'>
+						Visit GitHub Profile{' '}
+					</a>
+					<ul>
+						<li>
+							{login && (
+								<Fragment>
+									{' '}
+									UserName :<strong> {login}</strong>{' '}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{company && (
+								<Fragment>
+									{' '}
+									Company :<strong> {company}</strong>{' '}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{blog && (
+								<Fragment>
+									{' '}
+									Website :<strong> {blog}</strong>{' '}
+								</Fragment>
+							)}
+						</li>
+					</ul>
 				</div>
-				<Repos repos={repos} />
-			</Fragment>
-		);
-	}
-}
+			</div>
+			<div className='card text-center'>
+				<div className='badge badge-primary'>Followers : {followers}</div>
+				<div className='badge badge-success'>Following : {following}</div>
+				<div className='badge badge-light'>Public Repositories : {public_repos}</div>
+				<div className='badge badge-dark'>Public Gists : {public_gists}</div>
+			</div>
+			<Repos repos={repos} />
+		</Fragment>
+	);
+};
 
 export default User;
